4966b7811b20ddb9958feb7476b4481ff36f3eae
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-16 18:59:33 +0100

    auth

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -1,14 +1,3 @@
-package fact.it.apigateway.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -17,13 +6,11 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/album")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated()
+                        exchange.pathMatchers("/artiest").permitAll()  // Allow unauthenticated access to /products
+                                .anyExchange().authenticated()  // Require authentication for all other routes
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(withDefaults())
+                        .jwt(withDefaults())  // JWT token validation
                 );
         return serverHttpSecurity.build();
     }
