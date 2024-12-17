7aae14409795b2ed40a18da043726e435a1805b7
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-17 15:23:37 +0100

    auth

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -1,5 +1,5 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+package fact.it.apigateway.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -13,16 +13,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
-
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        logger.info("Configuring security filter chain");
         serverHttpSecurity
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers(HttpMethod.GET, "/album").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/winkel").authenticated() // Secure /winkel
-                        .pathMatchers(HttpMethod.GET, "/artiest").authenticated() // Secure /winkel
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
