50198553d0864603cd1e4849295602a2cd9d3632
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-17 16:26:30 +0100

    auth

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -16,10 +16,11 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers(HttpMethod.GET, "/album").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/winkel").permitAll()
-                        .anyExchange().authenticated()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(HttpMethod.GET,"/album")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults())
