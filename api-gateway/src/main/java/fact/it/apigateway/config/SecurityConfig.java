c8ac4026f4741801b0f200601bfb44a8db12d26f
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-17 18:56:10 +0100

    auth

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -16,11 +16,23 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
-                .authorizeExchange(exchange ->
-                        exchange.pathMatchers(HttpMethod.GET,"/album")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated()
+                .authorizeExchange(exchange -> exchange
+                        // Allow GET requests to /album, /winkel, and /artiest
+                        .pathMatchers(HttpMethod.GET, "/album", "/winkel", "/artiest")
+                        .permitAll()
+
+                        // Secure PUT, POST, and DELETE requests for /album, /winkel, and /artiest
+                        .pathMatchers(HttpMethod.PUT, "/album", "/winkel", "/artiest")
+                        .authenticated()
+
+                        .pathMatchers(HttpMethod.POST, "/album", "/winkel", "/artiest")
+                        .authenticated()
+
+                        .pathMatchers(HttpMethod.DELETE, "/album", "/winkel", "/artiest")
+                        .authenticated()
+
+                        // Require authentication for all other routes
+                        .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(withDefaults())
