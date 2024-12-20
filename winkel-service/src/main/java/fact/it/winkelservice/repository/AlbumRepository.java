fbf87321e992b0ea37ab352545aa28c015811b9d
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-20 20:20:56 +0100

    dtos artiest/winkel

--- winkel-service/src/main/java/fact/it/winkelservice/repository/AlbumRepository.java
+++ winkel-service/src/main/java/fact/it/winkelservice/repository/AlbumRepository.java
@@ -1,6 +1,6 @@
 package fact.it.winkelservice.repository;
 
-import fact.it.albumservice.model.Album;
+import fact.it.winkelservice.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
