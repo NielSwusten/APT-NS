8b306e741f4bf59896a3c2423d90344f426bff0c
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-20 20:24:55 +0100

    dtos artiest/winkel

--- artiest-service/src/main/java/fact/it/artiestservice/dto/ArtiestResponse.java
+++ artiest-service/src/main/java/fact/it/artiestservice/dto/ArtiestResponse.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ArtiestResponse {
-    private String id;
+    private Long id;
     private String name;
     private String description;
     private LocalDate dateOfBirth;
