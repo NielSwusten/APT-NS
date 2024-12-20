a6825851eae63658eaa0d92929faa2bae7418fa2
Niel Swusten <r0938850@student.thomasmore.be>
2024-12-20 22:44:19 +0100

    unit tests

--- artiest-service/src/test/java/fact/it/artiestservice/ArtiestServiceUnitTests.java
+++ artiest-service/src/test/java/fact/it/artiestservice/ArtiestServiceUnitTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.time.LocalDate;
 import java.util.List;
--- winkel-service/src/test/java/fact/it/winkelservice/WinkelServiceUnitTests.java
+++ winkel-service/src/test/java/fact/it/winkelservice/WinkelServiceUnitTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 import java.util.Optional;
